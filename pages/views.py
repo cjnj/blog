79a65f36903f032b0bc140e7addc51a22cc14160
COREY FSDI-111 <Corey.Jonhson@sdgku.edu>
2024-03-19 23:38:48 -0400

    Blog 112/113

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+
+
+# Create your views here.
+
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "templates/pages/home.html"
+        
+class AboutPageView(TemplateView):
+    template_name = "templates/pages/about.html"
