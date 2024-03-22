eef6f77a99ca412fc0261ef805626cb56385eb75
COREY FSDI-111 <Corey.Jonhson@sdgku.edu>
2024-03-21 23:12:05 -0400

    113 assignment-1-3/21/24

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+ # Create your views here.
 
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
-    template_name = "templates/pages/home.html"
+     template_name = "pages/home.html"
         
 class AboutPageView(TemplateView):
-    template_name = "templates/pages/about.html"
+     template_name = "pages/about.html"
+    
+    
+    
--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,11 @@ from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
+    UpdateView,
+    DeleteView
 )
 from .models import Post
+from django.urls import reverse_lazy
 
 class PostListView(ListView):
     template_name = "posts/list.html"
@@ -25,3 +28,12 @@ class PostCreateView(CreateView):
     model = Post
     fields = ["title", "subtitle", "body", "author"]
     
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+    
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
